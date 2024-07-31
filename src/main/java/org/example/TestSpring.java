d066e3e1de5a715a01792430f61dcc04a8e18143
RomanHiruko <sushenko.roman@mail.ru>
2024-07-31 22:12:03 +0300

    Урок 7 (Bean scope)

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -6,9 +6,11 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getVolume());
-        System.out.println(musicPlayer.getName());
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer == musicPlayer2);
+//        musicPlayer.playMusic();
+//        System.out.println(musicPlayer.getVolume());
+//        System.out.println(musicPlayer.getName());
         context.close();
     }
 }
