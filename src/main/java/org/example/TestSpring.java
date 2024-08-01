36756766da2ddc72735643b782759dac685760c8
RomanHiruko <sushenko.roman@mail.ru>
2024-08-01 13:43:48 +0300

    Урок 9 (Аннотации @component)

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -5,12 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
-        System.out.println(musicPlayer == musicPlayer2);
+        Music music = context.getBean("classicalMusic", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
         musicPlayer.playMusic();
-        System.out.println(musicPlayer.getVolume());
-        System.out.println(musicPlayer.getName());
         context.close();
     }
 }
