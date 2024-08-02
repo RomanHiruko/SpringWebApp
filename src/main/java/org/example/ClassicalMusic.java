d5b6321ab984ca00eef656f84ed9c2ad90e67aa7
RomanHiruko <sushenko.roman@mail.ru>
2024-08-02 09:08:15 +0300

    Урок 11 (Аннотация @Qualifier)

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,7 +1,7 @@
 package org.example;
 
 import org.springframework.stereotype.Component;
-
+@Component
 public class ClassicalMusic implements Music {
     private ClassicalMusic() {
     }
--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,22 +1,26 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private Music music;
+    @Qualifier("classicalMusic")
+    private Music music1;
+    private Music music2;
 
     public MusicPlayer() {
     }
 
     @Autowired
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,
+                       @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
-        return "Playing: " + music.getSong();
+        return "Playing: " + music1.getSong() + " " + music2.getSong();
     }
 }
